324da2b90009ee345308fcf32345f53129f73833
Clairine <clairine.christabel@ui.ac.id>
2026-03-10 15:58:44 +0700

    [GREEN] Implement PaymentRepository

--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepositoryTest.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
+import id.ac.ui.cs.advprog.eshop.model.Payment;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.HashMap;
