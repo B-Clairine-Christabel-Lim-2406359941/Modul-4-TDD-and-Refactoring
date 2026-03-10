9392e6d38f53c37f899172d535a03b5b50084faa
Clairine <clairine.christabel@ui.ac.id>
2026-03-10 17:39:59 +0700

    [RED] Implement functional tests for Order feature

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/functional/OrderFunctionalTest.java
@@ -0,0 +1,50 @@
+package id.ac.ui.cs.advprog.eshop.functional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class OrderFunctionalTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testGetCreateOrderPage() throws Exception {
+        mockMvc.perform(get("/order/create"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testGetOrderHistoryPage() throws Exception {
+        mockMvc.perform(get("/order/history"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testPostOrderHistory() throws Exception {
+        mockMvc.perform(post("/order/history")
+                        .param("author", "clairine.christabel"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testGetPaymentOrderPage() throws Exception {
+        mockMvc.perform(get("/order/pay/ord-123"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testPostPaymentOrder() throws Exception {
+        mockMvc.perform(post("/order/pay/ord-123"))
+                .andExpect(status().isOk());
+    }
+}
